6335d1122b4ee34aabe5cc5bd51257c521b562b8
Salih Sarikaya <salihsarikaya34@gmail.com>
2020-09-14 03:28:44 -0700

    Updates

---
+++ precache-manifest.dc52a8f0a9b379621824e45ec5da1fed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "769a562f2994143f1b22b146e5b2544d",
+    "url": "/CampAlley/index.html"
+  },
+  {
+    "revision": "0a943f6051f50c1f8e1d",
+    "url": "/CampAlley/static/css/2.bb926a4b.chunk.css"
+  },
+  {
+    "revision": "0a943f6051f50c1f8e1d",
+    "url": "/CampAlley/static/js/2.fe0c0573.chunk.js"
+  },
+  {
+    "revision": "97803cca9dfd19e7fa5f",
+    "url": "/CampAlley/static/js/main.7f18c81e.chunk.js"
+  },
+  {
+    "revision": "c217da68c5fbd4caebe5",
+    "url": "/CampAlley/static/js/runtime~main.0e707291.js"
+  }
+]);
