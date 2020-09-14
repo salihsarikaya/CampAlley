f9adbe34356a09cea5eafb346bce420f500f1b0c
Salih Sarikaya <salihsarikaya34@gmail.com>
2020-09-14 01:53:26 -0700

    firstcommit

---
+++ build/precache-manifest.0424068d70fe770711d37504af0b3809.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d0fe0fa0981a0cba3ec2c2b22747dc8",
+    "url": "/demos/themes/html/direo-react/index.html"
+  },
+  {
+    "revision": "0d33d57e2bcba94e74f1",
+    "url": "/demos/themes/html/direo-react/static/css/2.02454bce.chunk.css"
+  },
+  {
+    "revision": "0d33d57e2bcba94e74f1",
+    "url": "/demos/themes/html/direo-react/static/js/2.020d5f12.chunk.js"
+  },
+  {
+    "revision": "f5bf9244c186844b83ad",
+    "url": "/demos/themes/html/direo-react/static/js/main.7694e775.chunk.js"
+  },
+  {
+    "revision": "72b4d9e6a1f061141cbe",
+    "url": "/demos/themes/html/direo-react/static/js/runtime~main.01fde43a.js"
+  }
+]);
